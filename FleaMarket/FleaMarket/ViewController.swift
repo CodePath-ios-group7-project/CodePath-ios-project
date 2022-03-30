02aafa5904028f5bf5dfd87f3331e44db5fdd5a2
iris-w03 <93733201+iris-w03@users.noreply.github.com>
2022-03-30 16:22:43 -0500

    Part 1
    Add welcome screen, register& login

---
+++ FleaMarket/FleaMarket/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FleaMarket
+//
+//  Created by Ziyue Wang on 3/30/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ FleaMarket/Pods/Target Support Files/Pods-FleaMarket-FleaMarketUITests/Pods-FleaMarket-FleaMarketUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FleaMarket_FleaMarketUITests : NSObject
+@end
+@implementation PodsDummy_Pods_FleaMarket_FleaMarketUITests
+@end
